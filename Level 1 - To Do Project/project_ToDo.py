9dc57105690778b899ca3a07d6fe7751746f9ceb
Leandro <leandroudiprado@gmail.com>
2024-07-22 16:39:47 -0300

    updating somethings in to_do_project

--- Level 1 - To Do Project/project_ToDo.py
+++ Level 1 - To Do Project/project_ToDo.py
@@ -7,9 +7,12 @@ def add_task(task_list, task):
     return
 
 def see_tasks(task_list):
+    print('---- TO DO LIST ----')
     for index, task in enumerate(task_list, start=1):
         check_status = '✓' if task["status"] else ' '
         print(f'{index}. [{check_status}] {task["task_name"]}')
+    print('--------------------')
+
 
 
 def update_task(task_list, task_index, new_task):
@@ -39,26 +42,31 @@ def delete_checked_taks(task_list):
     return
 
 while True:
-    print("\nTO DO's TASKS MENU")
+    print("\n---- TO DO's TASK MENU ----")
     print("[1] - Add a Task")
     print("[2] - See your Tasks")
     print("[3] - Update a Task")
     print("[4] - Check a Task")
     print("[5] - Delete checked Tasks")
     print("[6] - Exit")
+    print("----------------------------")
 
-    choice = int(input("What do you wanna do? "))
+    choice = int(input("\nWhat is your choice? "))
 
     if choice == 6:
         print('\nThe program has finished. See you!\n')
         break
     
     elif choice == 1:
-        task_name = str(input("\nWhat do you have TO DO? ")).strip().title()
+        task_name = str(input("\nWhat do you need TO DO? ")).strip().title()
         add_task(task_list,task_name)
         
     elif choice == 2:
-        see_tasks(task_list)
+        if len(task_list) == 0:
+            print(f"You have NO task yet.")
+
+        else:
+            see_tasks(task_list)
 
     elif choice == 3:
         see_tasks(task_list)
