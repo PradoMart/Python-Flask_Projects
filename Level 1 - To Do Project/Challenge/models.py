c7d7e36a5fdd950c3405865572ad676d8aaa66d4
Leandro <leandroudiprado@gmail.com>
2024-07-24 10:34:59 -0300

    feat: implemeting the program's funcionalities

---
+++ Level 1 - To Do Project/Challenge/models.py
@@ -0,0 +1,88 @@
+def titles (message, color = 'BLUE', tam = int(35)):
+    global tamanho
+    tamanho = tam
+    colors('-'*tamanho, color)
+    colors(f'{str(message).upper().center(tamanho)}',color)
+    colors('-'*tamanho, color)
+    return
+
+def colors(message, color):
+    if color == 'BLUE':
+        print(f"\033[1;34m{message}\033[m")
+    elif color == 'YELLOW':
+        print(f"\033[1;33m{message}\033[m")
+    elif color == 'RED':
+        print(f"\033[1;31m{message}\033[m")
+    elif color == 'GREEN':
+        print(f"\033[1;32m{message}\033[m")
+    elif color == 'MAGENTA':
+        print(f"\033[1;35m{message}\033[m")
+    elif color == 'CYAN':
+        print(f"\033[1;36m{message}\033[m")
+    return
+
+def menu():
+    print()
+    titles('MENU')
+    print("""[1] - Add a contact
+[2] - See all contacts
+[3] - Edit a contact
+[4] - To favorite a contact
+[5] - See only favorite contacts
+[6] - Delete a contact
+[7] - Exit""")
+    colors('-'*tamanho,'BLUE')
+
+    return
+
+def add_contact (list, name, phone, email, favorited):
+    list.append({
+                "name": name,
+                 "phone": phone,
+                 "e-mail": email,
+                 "favorite": favorited
+                 })
+    colors(f"\n{str(name).upper()} was added!",'GREEN')
+    colors('-'*tamanho, 'GREEN')
+    return  
+    
+def see_contacts(list, message, favorite = False, color = 'BLUE'):
+    if not favorite:
+        titles(f'{message}',color , tam = int(83))
+        print(f'\033[1m{"INDEX":<10}{"NAME":<20}{"PHONE":<15}{"E-MAIL":<30}{"FAVORITE"}\033[m')
+        for index, v in enumerate(list,start=1):
+            print(f" [{index}]{"":<6}{v["name"]:<20}{v["phone"]:<15}{v["e-mail"]:<30}{v["favorite"]:^8}")
+        colors('-'* tamanho, color)
+        print()
+    else:
+        titles(f'{message}', color, tam = int(83))
+        print(f'\033[1m{"INDEX":<10}{"NAME":<20}{"PHONE":<15}{"E-MAIL":<30}{"FAVORITE"}\033[m')
+        for index, v in enumerate(list,start=1):
+            if v["favorite"] == 'Y':
+                print(f" [{index}]{"":<6}{v["name"]:<20}{v["phone"]:<15}{v["e-mail"]:<30}{v["favorite"]:^8}")
+        colors('-'* tamanho, color)
+        print()
+
+    return
+
+def edit_contacts(list, index, name, phone, email, favorited):
+    list[index]["name"] = name
+    list[index]["phone"] = phone
+    list[index]["e-mail"] = email
+    list[index]["favorite"] = favorited
+    colors(f'\nContact updated!','YELLOW')
+    return
+
+def edting_some_info (list, index, info_check, new_info):
+    if info_check == 'name':
+        new_info = str(new_info).title()
+    elif info_check == 'favorite':
+        new_info = str(new_info).upper()[0]
+    list[index][f"{info_check}"] = new_info
+    colors(f"\n{str(info_check).upper()}'S contact updated!")
+    return
+
+def favorite_contact(list, index):
+    list[index]["favorite"] = 'Y'
+    colors(f"{str(list[index]["name"]).upper()} is now a favorite contact!",'MAGENTA')
+    return
---
+++ Level 1 - To Do Project/Challenge/project.py
@@ -0,0 +1,73 @@
+from models import*
+
+contacts_list =[]
+
+while True:
+    menu()
+    try:
+        print()
+        choice = int(input('\033[1mWhat do you like to do? \033[m'))
+        print()
+        
+        if choice == 7:
+            titles('The program has finished. see you!','YELLOW')
+            break
+        
+        elif choice == 1:
+            titles('ADDING CONTACT','GREEN')
+            name = str(input("What's the contact's name? ").strip().title())
+            phone = str(input("What about the phone: ").strip())
+            email = str(input("And now the e-mail: ").strip())
+            favorite = str(input("Would you like to favorite this contact?\nType only [YES] or [NO]: ").strip().upper())[0]
+            while favorite not in 'YN':
+                favorite = str(input("\033[1;33mSorry, option invalid!\033[m Try again [YES / NO]: ").strip().upper())[0]
+            add_contact(contacts_list, name, phone, email, favorite)
+
+        elif choice == 2:
+            see_contacts(contacts_list, "CONTACT'S LIST", color='CYAN')
+
+        elif choice == 3:
+            see_contacts(contacts_list, 'EDTING CONTACT', color= 'YELLOW')
+            index_contact = int(input('What contact do you wanna edit? ')) - 1 
+            titles('NEW INFORMATIONS', color= 'YELLOW', tam= int(83))
+            
+            check_changes = str(input(f"Would you like to edit all {str(contacts_list[index_contact]["name"]).upper()}'S infos ? [YES / NO] ")).strip().upper()[0]
+            while check_changes not in 'YN':
+                check_changes = str(input(f"\033[1;33mSorry, option invalid!\033[m Try again [YES / NO]: ")).strip().upper()[0]
+
+            if check_changes == 'N':
+                print(f"Here's the informations you can edit:", end= ' ')
+                for k, v in enumerate(contacts_list):
+                    if k == 0:
+                        print(f"\033[1m{v.keys()}\033[m")
+                    pass
+                
+                variable_check = str(input("What info would you like to edit? ")).strip().lower()
+                while variable_check not in v.keys():
+                    variable_check = str(input("\033[1;33mSorry, option invalid!\033[m Try again [YES / NO]: ")).strip().lower()
+                new_info = input("Write the new information: ").strip()
+                edting_some_info(contacts_list, index_contact, variable_check, new_info)
+            else:
+                new_name = str(input("What's the contact's \033[1mNEW name\033[m? ").strip().title())
+                new_phone = str(input("What about the \033[1mNEW phone\033[m: ").strip())
+                new_email = str(input("And now, what's the \033[1mNEW e-mail\033[m: ").strip())
+                new_favorite = str(input("Would you like to favorite this contact? Type only [YES] or [NO]: ").strip().upper())[0]
+                edit_contacts(contacts_list, index_contact, new_name, new_phone, new_email, new_favorite)
+            colors('-' * 83, 'YELLOW')
+        
+        elif choice == 4:
+            see_contacts(contacts_list, 'FAVORITING A CONTACT', color= 'MAGENTA')
+            index_contact = int(input('What contact do you wanna favorite? ')) - 1
+            favorite_contact(contacts_list, index_contact)
+
+        elif choice == 5:
+            see_contacts(contacts_list, "FAVORITE CONTACT'S LIST", True, 'MAGENTA')
+        
+        elif choice == 6:
+            see_contacts(contacts_list, 'DELETING A CONTACT', color='RED')
+            index_contact = int(input('What contact do you wanna delete? ')) - 1
+            del contacts_list[index_contact]
+            colors('Contact deleted!','RED')
+
+    except Exception as problem:
+        colors(f"Something went wrong! Here's the problem: {str(problem).upper()}",'YELLOW')
