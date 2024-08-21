add1d7c9f8dbd1704c7a02521461f2476eee0408
Leandro <leandroudiprado@gmail.com>
2024-08-20 21:57:37 -0300

    importing lib login_user and current_user to auth the user

--- Level 4 - Flask-Auth-SQLite/Class Lesson/app.py
+++ Level 4 - Flask-Auth-SQLite/Class Lesson/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.database import db
 from models.user import User
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -11,9 +11,13 @@ db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-@app.route("/hello-world", methods=['GET'])
-def hello_world():
-    return "Hello World"
+#view login
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -22,12 +26,15 @@ def login():
     password = data.get("password")
 
     if username and password:
+        #Login
         user = User.query.filter_by(user_name=username).first()
+        
         if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
             return jsonify({"message":"User authenticated."})
 
-    else:
-        return jsonify({"message":"User not found."}), 400
+    return jsonify({"message":"User not found."}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
