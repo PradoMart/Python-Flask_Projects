0694aa5433a0bf284e8dfe0af244aa5c42b4d0b8
Leandro <leandroudiprado@gmail.com>
2024-08-26 20:15:39 -0300

    implementing the logout route

--- Level 4 - Flask-Auth-SQLite/Class Lesson/app.py
+++ Level 4 - Flask-Auth-SQLite/Class Lesson/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.database import db
 from models.user import User
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, logout_user,current_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -35,6 +35,13 @@ def login():
             return jsonify({"message":"User authenticated."})
 
     return jsonify({"message":"User not found."}), 400
+
+@app.route("/logout", methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "User loged out."})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
